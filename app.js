8201e253c133da29394aa1964a733b32fc9afc39
AleksandrV <kolomel@yandex.ru>
2020-09-26 09:58:42 +0300

    add schemes & models: user, card

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const routeCards = require('./routes/cards.js');
 const routeUsers = require('./routes/users.js');
@@ -6,6 +7,13 @@ const routeUsers = require('./routes/users.js');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
 
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/users', routeUsers);
